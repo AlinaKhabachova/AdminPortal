701e73b97f3c39268f9505b772bba5223ac1cd6c
AlinaKhabachova <alina.khabachyova@gmail.com>
2014-10-15 11:41:31 +0300

    fix

--- src/main/java/com/alina/common/HelloBean.java
+++ src/main/java/com/alina/common/HelloBean.java
@@ -2,22 +2,40 @@ package com.alina.common;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import java.io.IOException;
 import java.io.Serializable;
- 
-@ManagedBean
-(name = "helloBean")
+
+@ManagedBean(name = "helloBean")
 @SessionScoped
-public class HelloBean implements Serializable {
-      
+public class HelloBean implements Serializable
+{
+    
     private static final long serialVersionUID = 3642598894525500326L;
-    private String name;
- 
-    public String getName() 
+    
+    private String            name;
+    
+    public String getName()
     {
         return name;
     }
+    
     public void setName(String name)
     {
         this.name = name;
+    }
+    
+    public void redirectToWeclome()
+    {
+        try
+        {
+            FacesContext.getCurrentInstance().getExternalContext()
+                    .redirect("Welcome.xhtml");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
