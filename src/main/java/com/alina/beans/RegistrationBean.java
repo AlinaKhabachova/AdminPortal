f24de96718346467e8c6ca26bf1ab97f07fdf655
AlinaKhabachova <alina.khabachyova@gmail.com>
2015-01-27 20:30:53 +0200

    validation

--- src/main/java/com/alina/beans/RegistrationBean.java
+++ src/main/java/com/alina/beans/RegistrationBean.java
@@ -12,32 +12,37 @@ public class RegistrationBean
 {
     private String rePassword;
     
-    private User user = new User();
+    private User   user = new User();
     
     public void save()
     {
-        if(user.getPassword().equals(rePassword))
-            IndexedEntityService.add(user);
+        if (!"".equals(user.getPassword()))
+        {
+            if (user.getPassword().equals(rePassword))
+                IndexedEntityService.add(user);
+        }
         else
+        {
             System.out.println("wrong password");
+        }
         
     }
     
     public User getUser()
     {
         return user;
-    }    
+    }
     
     public void setUser(User user)
     {
         this.user = user;
     }
-
+    
     public String getRePassword()
     {
         return rePassword;
     }
-
+    
     public void setRePassword(String rePassword)
     {
         this.rePassword = rePassword;
